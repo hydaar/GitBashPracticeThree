7a86dfdb191fbd9d315260aea0e6d2c4c9090447
hydaar <hyders888@gmail.com>
2022-07-29 11:50:04 +0530

    first commit

---
+++ src/main/java/practiceproject/allurepracticeTest.java
@@ -0,0 +1,13 @@
+package practiceproject;
+
+import org.testng.annotations.Test;
+
+public class allurepracticeTest {
+
+	
+	@Test
+	public void test()  {
+		
+		System.out.println("hi this is the TEST CASE 1");
+	}
+}
