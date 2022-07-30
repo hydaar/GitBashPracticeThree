aa7c8350e66481662242a2a0b4cf522a835447de
hydaar <hyders888@gmail.com>
2022-07-30 19:54:39 +0530

    modified on july 30 satureday

---
+++ src/main/java/practiceproject/allurepracticeTest2.java
@@ -0,0 +1,17 @@
+package practiceproject;
+
+import org.testng.annotations.Test;
+
+public class allurepracticeTest2 {
+
+	
+	@Test
+	public void test()  {
+		
+		System.out.println("hi this is the TEST CASE 1");
+	System.out.println("hi this is the TEST CASE 1");
+	System.out.println("hi this is the TEST CASE 1");
+	System.out.println("hi this is the TEST CASE 1");
+	
+}
+}
